6aedbdc1a2418bb96c85d76fef9756db8d06094f
Harry <hfitz83@gmail.com>
2018-11-06 11:10:05 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Express Template
+
+## Setup:
+1. `npm install`
+2. `npm run start` (look in package.json for more insight)
+
+## Extending
+1. Learn express! 
+  - https://expressjs.com/
+  - https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+2. Install some packages from https://www.npmjs.com/ and hack away!
---
+++ index.js
@@ -0,0 +1,20 @@
+// imports
+const express = require('express');
+const path = require('path');
+
+// server config
+const public = path.join(__dirname, 'public');
+const port = process.env.PORT || 8080;
+
+// create a server
+const server = express();
+
+// on any GET request, look in public/ for the corresponding file
+server.get(express.static(public));
+
+// set up pathways to access a database 
+server.get('/api/database', (req, res) => {
+  res.send(200);
+});
+
+server.listen(port, () => console.log(`server listening on http://localhost:${port}`));
